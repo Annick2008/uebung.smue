3b28650fbe7f807fd407d4981e4577826a0716ec
Yannick Kreuter <y.kreuterle@sbg.at>
2023-12-05 19:54:46 +0100

    new exercise

---
+++ Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uebung.smue;
+
+public class Auto
+{
+	private string _marke;
+
+	public string Marke
+	{
+		get { return _marke; }
+		set { _marke = value; }
+	}
+
+	private string _modell;
+
+	public string Modell
+	{
+		get { return _modell; }
+		set { _modell = value; }
+	}
+
+	public double Hoehe { get; set; }
+
+	public double Laenge { get; set; }
+
+	public bool AutoOffenZu {  get; private set; }
+
+	public void AutoOffen()
+	{
+		AutoOffenZu = true;
+	}
+
+	public void AutoZu()
+	{
+		AutoOffenZu = false;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using uebung.smue;
+
+Auto a1 = new Auto();
+a1.Marke = "BMW";
+a1.Modell = "M5 Competition";
+a1.Hoehe = 2.56;
+a1.Laenge = 4.89;
+a1.AutoOffen();
+a1.AutoZu();
+a1.AutoOffen();
+
+if(a1.AutoOffenZu ==  true)
+{
+    Console.WriteLine("Das Auto ist gerade aufgesperrt.");
+}
+else
+{
+    Console.WriteLine("Das Auto ist gerade zugesperrt.");
+}
+
+Console.WriteLine("Die Marke und das Modell des Autos heißen {0} {1}. Die Höhe ist {2} cm und die Breite ist " +
+    "{3} cm", a1.Marke, a1.Modell, a1.Hoehe, a1.Laenge);
